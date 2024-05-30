9d9111a82347787650de6f5d2fb11eb2fd13e732
AnastasiaTomar27 <anastasia.tomar@edu.keuda.fi>
2024-05-30 13:43:57 +0300

    30.5

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -15,12 +15,41 @@ describe('Testing routs', () => {
         expect(response.statusCode).toBe(200);
         expect(response.body.message).toBe(`The product of submitted numbers is ${2*4*88}.`);
     });
+
     test('should respond with a 400 status code and a message "Only numbers can be submitted!"', async () => {
         const response = await request(app).post("/api/product").send({
             "numbers": [2, 4, "t"]
         });
         expect(response.statusCode).toBe(400);
         expect(response.body.message).toBe("Only numbers can be submitted!");
+    });
+
+    test('should return total price without discount', async () => {
+        const response = await request(app).post("/api/prices").send({
+            "prices": [3, 14.50, 10]
+        });
+        expect(response.body.message).toBe(`Total price is ${3 + 14.50 + 10} €.`);
+    });
+
+    test('should return total price with discount 10%', async () => {
+        const response = await request(app).post("/api/prices").send({
+            "prices": [3, 14, 100]
+        });
+        expect(response.body.message).toBe(`Total price after discount is ${((3 + 14 + 100) * 0.1).toFixed(2)} €.`);
+    });
+
+    test('should return a message that email is valid', async () => {
+        const response = await request(app).post("/api/email_address").send({
+            "email": "anastasia@keuda.fi"
+        });
+        expect(response.body.message).toBe("anastasia@keuda.fi is valid");
+    });
+
+    test('should return a message that email is invalid', async () => {
+        const response = await request(app).post("/api/email_address").send({
+            "email": "anastasia(@keuda.fi"
+        });
+        expect(response.body.message).toBe("anastasia(@keuda.fi is invalid");
     })
 });
    
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
-app.use(express.json())
+const bodyParser = require('body-parser');
+//const path = require('path');
+//const dotenv  = require('dotenv');
+
+//dotenv.load({  path: '.env' });
+
+app.use(express.json()); // middleware will process JSON  data sent by client
+
+app.use(bodyParser.json()); // this is a middleware. It parse JSON Request Bodies
+//app.use(bodyParser.urlencoded({ extended: true })); // Parses data sent via HTML forms
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -11,8 +21,6 @@ app.get("/", (req, res) => {
 app.post("/api/product", (req, res) => {
     const { numbers } = req.body;
     const product = numbers.reduce((accumulator, element) => accumulator * element, 1)
-    //res.json({ message:  `The product of submitted numbers is ${product}.`});
-    //console.log(req.body);
     var allAreNumbers = true;
     numbers.forEach(number => {
         const parsedNumber = parseInt(number);
@@ -22,14 +30,29 @@ app.post("/api/product", (req, res) => {
         }
     })
     if (allAreNumbers === false) return res.status(400).send({message: "Only numbers can be submitted!"})
-    else return res.json({ message:  `The product of submitted numbers is ${product}.`});
-        
+    else return res.json({ message:  `The product of submitted numbers is ${product}.`}); 
+})
+
+
+
+app.post("/api/prices", (req, res) => {
+    const { prices } = req.body;
+    const total_price = prices.reduce((accumulator, price) => accumulator + price)
+    if (total_price > 100) return res.json({message: `Total price after discount is ${(total_price * 0.1).toFixed(2)} €.`});
+    else return res.json({message: `Total price is ${total_price} €.`})
     
 })
 
+app.post("/api/email_address", (req, res) => {
+    const { email } = req.body;
+    if (email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) return res.json({message: `${email} is valid`});
+    else return res.status(400).send({message: `${email} is invalid`});
+    
+});
+
 app.use((err, req, res, next) => {
     return res.status(500).json({ msg: err.msg || err.message, code: err.code })
-});
+}); // integrates middleware in all routes
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
